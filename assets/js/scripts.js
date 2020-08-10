b74bc8d9e216ca5ee7b4b8a9e684a0a0d9181fc9
Isaque Moura <improgramcode@gmail.com>
2020-08-10 18:33:36 -0300

    Inclusão de banco de dados simples para capturar email do interessado

---
+++ assets/js/scripts.js
@@ -0,0 +1,27 @@
+/*let email = document.querySelector("#exampleInputEmail1");
+let form = document.querySelector("#form");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let dados = {
+        email: exampleInputEmail.value,
+    };
+
+    fetch('')
+
+})*/
+
+function envio(){
+
+    var email = document.getElementById('exampleInputEmail1').nodeValue;
+
+    $.ajax({
+
+        method: "POST",
+        url: "http://localhost:3000/user;"
+
+    })
+
+}
+
