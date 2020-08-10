8c778badd39208807a679d6e2342badfca6d6956
Isaque Moura <improgramcode@gmail.com>
2020-08-10 18:44:50 -0300

    Editando requisição

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,7 +11,7 @@ form.addEventListener("submit", function(event) {
     fetch('')
 
 })*/
-
+/*
 function envio(){
 
     var email = document.getElementById('exampleInputEmail1').nodeValue;
@@ -25,3 +25,4 @@ function envio(){
 
 }
 
+*/
